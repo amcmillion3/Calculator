6ac981fc7b711ea84805416d56f70a928415f006
Andrew McMillion <amcmillion3@gmail.com>
2021-05-06 11:15:01 +0200

    Finish but damn

--- app.js
+++ app.js
@@ -14,23 +14,78 @@ class Calculator {
     };
 
     delete() {
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
     };
 
+    // Must convert numbers to string so JS doesn't just add the numbers together
     appendNumber(number) {
-        this.currentOperand = number;
+        if (number === "." && this.currentOperand.includes(".")) return;
+        this.currentOperand = this.currentOperand.toString() + number.toString();
     };
 
     selectOperation(operation) {
-
+        if (this.currentOperand === "") return;
+        if (this.previousOperand !== "") {
+            this.operate();
+        };
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = "";
     };
     
     operate() {
-
+        let computation
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operation) {
+            case "+":
+                computation =  prev + current;
+                break;
+            case "-":
+                computation =  prev - current;
+                break;
+            case "x":
+                computation =  prev * current;
+                break;
+            case "÷":
+                computation =  prev / current;
+                break;
+            case "x<sup>y</sup>":
+                computation =  prev ** current;
+                break;
+            default:
+                return;
+        };
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = "";
     };
-     
+
+    getDisplayNumber(number) {
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split(".")[0]);
+        const decimalDigits = stringNumber.split(".")[1];
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = "";
+        } else {
+            integerDisplay = integerDigits.toLocaleString("en", {maximumFractionDigits: 0});
+        };
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`;
+        } else {
+            return integerDisplay;
+        }
+    };
+
     updateDisplay() {
-        this.currentOperandTextElement.innerText = this.currentOperand;
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand);
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`;
+        } else {
+            this.previousOperandTextElement.innerText = "";
+        }
     };
 };
 
@@ -48,5 +103,27 @@ numberButtons.forEach(button => {
     button.addEventListener("click", () => {
         calculator.appendNumber(button.innerText);
         calculator.updateDisplay();
-    })
+    });
+});
+
+operationButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        calculator.selectOperation(button.innerText);
+        calculator.updateDisplay();
+    });
+});
+
+equalsButton.addEventListener("click", () => {
+    calculator.operate();
+    calculator.updateDisplay();
+});
+
+clearButton.addEventListener("click", () => {
+    calculator.clear();
+    calculator.updateDisplay();
+})
+
+deleteButton.addEventListener("click", () => {
+    calculator.delete();
+    calculator.updateDisplay();
 })
